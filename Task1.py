16791abf4083ab5fdd0fc420c431882bb1e27a6c
TatyanaAbramova <107611864+TatyanaAbramova@users.noreply.github.com>
2022-09-11 17:14:29 +0500

    Update Task1.py
    Change

--- Task1.py
+++ Task1.py
@@ -7,7 +7,5 @@ def sum_odd_index(lst):
             s += lst[i]
     print(f'Сумма равна: {s}')
 
-lst = [2, 3, 5, 9, 3]
-sum_odd_index(lst)
 lst = list(map(int, input('Введите числа через пробел:\n').split()))
 sum_odd_index(lst)
